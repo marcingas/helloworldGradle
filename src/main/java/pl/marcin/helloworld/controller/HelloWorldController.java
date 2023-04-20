796e0cc8a9c2ef84154b925f850a20d893f26e24
Marcin <gasiorek.mail@gmail.com>
2023-04-20 20:52:36 +0200

    Initial commit

---
+++ src/main/java/pl/marcin/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package pl.marcin.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+}
